51c5cab55d3c1f1e80551b482743e6dc7e24988e
giovanniPepi <gtpepi@proton.me>
2023-02-18 09:08:09 -0300

    Fix other instruments in populatedb; fix controllers; add home page view

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,24 +1,20 @@
 const Brand = require("../models/Brand");
-const async = require("async")
+const async = require("async");
 
-exports.brand_list = (req, res, next) = {}
+exports.brand_list = (req, res, next) => {};
 
-exports.brand_detail = (req, res, next) = {}
+exports.brand_detail = (req, res, next) => {};
 
-exports.brand_create_get = (req, res, next) = {}
+exports.brand_create_get = (req, res, next) => {};
 
-exports.brand_create_post = (req, res, next) = {}
+exports.brand_create_post = (req, res, next) => {};
 
-exports.brand_delete_get = (req, res, next) = {}
+exports.brand_delete_get = (req, res, next) => {};
 
-exports.brand_delete_post = (req, res, next) = {}
+exports.brand_delete_post = (req, res, next) => {};
 
-exports.brand_update_get = (req, res, next) = {}
+exports.brand_update_get = (req, res, next) => {};
 
-exports.brand_update_post = (req, res, next) = {}
+exports.brand_update_post = (req, res, next) => {};
 
-exports.brand_list = (req, res, next) = {};
-
-
-
-
+exports.brand_list = (req, res, next) => {};
