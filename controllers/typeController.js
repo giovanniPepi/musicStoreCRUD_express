abd6c64ade6e31944a76b12018bdca0de55ca683
giovanniPepi <gtpepi@proton.me>
2023-02-18 10:17:16 -0300

    Add instrument types

--- controllers/typeController.js
+++ controllers/typeController.js
@@ -1,8 +1,6 @@
 const Type = require("../models/Type");
 const async = require("async");
 
-exports.type_list = (req, res, next) => {};
-
 exports.type_detail = (req, res, next) => {};
 
 exports.type_create_get = (req, res, next) => {};
@@ -16,3 +14,20 @@ exports.type_delete_post = (req, res, next) => {};
 exports.type_update_get = (req, res, next) => {};
 
 exports.type_update_post = (req, res, next) => {};
+
+exports.type_list = (req, res, next) => {
+  // The request includes all the info we want to acess
+  // and is passed down to the pug view render
+  Type.find({}, "name description")
+    .sort({ title: 1 })
+    .populate("name")
+    .exec(function (err, list_types) {
+      if (err) {
+        return next(err);
+      }
+      res.render("type_list", {
+        title: "Instrument Types",
+        type_list: list_types,
+      });
+    });
+};
